96ccf73d7aefade64d58c397cee79e467e3e8d88
tzujuichou <tzujuichou@gmail.com>
2025-07-31 13:59:16 -0500

    more

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ st.set_page_config(layout="wide")
 # --- HIDE STREAMLIT STYLE ---
 hide_st_style = """
             <style>
-            #MainMenu {display: none;}
-            header {display: none;}
-            [data-testid="stFooter"] {display: none;}
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            [data-testid="stFooter"] {visibility: none;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
