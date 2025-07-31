5faddc168a9434a5356ad0b8ad6dcff508eba5b5
tzujuichou <tzujuichou@gmail.com>
2025-07-31 14:16:12 -0500

    20250731: another attempt to remove "hosted with streamlit"

--- app.py
+++ app.py
@@ -2,28 +2,27 @@ import streamlit as st
 import pandas as pd 
 st.set_page_config(layout="wide")
 
-# --- HIDE STREAMLIT STYLE ---
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            footer {
-            visibility: hidden;
-                }
-            footer:after {
-                content:'goodbye'; 
-                visibility: visible;
-                display: block;
-                position: relative;
-                #background-color: red;
-                padding: 5px;
-                top: 2px;
-            }
-            [data-testid="stStatusWidget"] {display: none;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+from streamlit.components.v1 import html
+
+# --- HIDE STREAMLIT BRANDING ---
+hide_streamlit_branding = """
+<style>
+    #MainMenu {display: none;}
+    header {display: none;}
+</style>
+"""
+st.markdown(hide_streamlit_branding, unsafe_allow_html=True)
+
+# Use the JavaScript snippet to hide the footer elements
+html('''
+    <script>
+        var body = window.parent.document.querySelector(".main");
+        var footer = body.querySelector("footer");
+        if (footer) {
+            footer.style.display = "none";
+        }
+    </script>
+''')
 
 pages = st.navigation([st.Page("Overview.py"), st.Page("Analysis.py")])
 pages.run()
