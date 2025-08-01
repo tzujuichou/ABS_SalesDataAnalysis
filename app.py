03f21cdf1c199c733c07d34404dc2d453885ec53
tzujuichou <tzujuichou@gmail.com>
2025-07-31 23:42:57 -0500

    another

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import streamlit as st
 import pandas as pd 
 
-hide_toolbar_style = """
+hide_specific_elements_style = """
             <style>
+            /* Hide the hamburger menu */
             #MainMenu {visibility: hidden;}
+            button[title="View app menu"] {visibility: hidden;}
+
+            /* Hide the GitHub and Fork buttons */
+            div[data-testid="stToolbar"] {
+                display: flex;
+                justify-content: flex-end; /* Aligns remaining items to the right */
+            }
+            div[data-testid="stToolbar"] a[title="View on GitHub"] {
+                display: none;
+            }
+            div[data-testid="stToolbar"] button[title="Fork"] {
+                display: none;
+            }
+
+            /* Hide the footer */
+            footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_toolbar_style, unsafe_allow_html=True)
+st.markdown(hide_specific_elements_style, unsafe_allow_html=True)
+
 
 pages = st.navigation([st.Page("Overview.py"), st.Page("Analysis.py")])
 pages.run()
