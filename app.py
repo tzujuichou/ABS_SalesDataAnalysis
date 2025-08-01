136488357bc29ef0d11fa43947997d33f15c46e9
tzujuichou <tzujuichou@gmail.com>
2025-07-31 23:56:12 -0500

    12

--- app.py
+++ app.py
@@ -1,39 +1,15 @@
 import streamlit as st
 import pandas as pd 
 
-# hide_specific_elements_style = """
-#             <style>
-#             #MainMenu {visibility: hidden;}
-#             button[title="View app menu"] {visibility: hidden;}
-#             div[data-testid="stToolbar"] {
-#                 display: flex;
-#                 justify-content: flex-end; 
-#             }
-#             footer {visibility: hidden;}
-#             </style>
-#             """
-
 hide_elements_style = """
             <style>
+            #MainMenu {visibility: hidden;}
+            button[title="View app menu"] {visibility: hidden;}
             div[data-testid="stToolbar"] {
-                right: 2rem;
+                display: flex;
+                justify-content: flex-end; 
             }
-
-            div[data-testid="stToolbar"] > button[title="View app menu"] {
-                display: none;
-            }
-            
-            div[data-testid="stToolbar"] > button[title="Fork"] {
-                display: none;
-            }
-
-            div[data-testid="stToolbar"] > a {
-                display: none;
-            }
-
-            footer {
-                display: none;
-            }
+            footer {visibility: hidden;}
             </style>
             """
 st.markdown(hide_elements_style, unsafe_allow_html=True)
