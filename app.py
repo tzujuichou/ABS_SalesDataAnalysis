7ccbdbb92a23f636cef56674b17ac666c0d78b06
tzujuichou <tzujuichou@gmail.com>
2025-08-02 09:42:14 -0500

    20250802: removing uncessary code

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
 import streamlit as st
 import pandas as pd 
 
-hide_elements_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            button[title="View app menu"] {visibility: hidden;}
-            div[data-testid="stToolbar"] {
-                display: flex;
-                justify-content: flex-end; 
-            }
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_elements_style, unsafe_allow_html=True)
-
-
 pages = st.navigation([st.Page("Overview.py"), st.Page("Analysis.py")])
 pages.run()
